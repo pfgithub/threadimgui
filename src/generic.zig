3eba0738fdba72e33891f159184db8828a265b8c
pfg <pfg@pfg.pw>
2021-04-09 01:33:50 -0700

    json parsing is very !fun and !enjoyable

--- src/generic.zig
+++ src/generic.zig
@@ -1,24 +1,58 @@
+const JsonHelper = struct {
+    value: ?std.json.Value,
+    alloc: *std.mem.Allocator,
+
+    pub fn subvalue(jh: JsonHelper, value: ?std.json.Value) JsonHelper {
+        return JsonHelper{ .value = value, .alloc = jh.alloc };
+    }
+
+    pub fn get(jh: JsonHelper, field: []const u8) JsonHelper {
+        return switch (jh.value orelse return jh.subvalue(null)) {
+            .Object => |obj| jh.subvalue(obj.get(field)),
+            else => jh.subvalue(null),
+        };
+    }
+    pub fn asString(jh: JsonHelper) ![]const u8 {
+        const value = jh.value orelse return error.BadJSON;
+        return switch (value) {
+            .String => |str| return str,
+            else => return error.BadJSON,
+        };
+    }
+    pub fn asEnum(jh: JsonHelper, comptime Enum: type) !Enum {
+        return std.meta.stringToEnum(Enum, try jh.asString()) orelse return error.BadJSON;
+    }
+    fn ReturnValueType(comptime fnc: anytype) type {
+        return @typeInfo(@typeInfo(@TypeOf(fnc)).Fn.return_type orelse unreachable).ErrorUnion.payload;
+    }
+    pub fn asArray(jh: JsonHelper, comptime fromJSON: anytype) ![]ReturnValueType(fromJSON) {
+        return switch (jh.value orelse return error.BadJSON) {
+            .Array => |arr| {
+                var res_array = try jh.alloc.alloc(ReturnValueType(fromJSON), arr.items.len);
+                for (arr.items) |itm, i| res_array[i] = try fromJSON(jh.subvalue(itm));
+                return res_array;
+            },
+            else => return error.BadJSON,
+        };
+    }
+};
+
 pub const Page = struct {
     title: []const u8,
     content: []const PostContext,
     sidebar: []const SidebarNode,
     display_mode: DisplayMode,
 
-    pub fn fromJSON(vt: std.json.Value) !Page {
-        switch (vt) {
-            .Object => |obj| {
-                return Page{
-                    .title = try stringFromJSON(obj.get("title")),
-                    .content = &[_]PostContext{},
-                    .sidebar = &[_]SidebarNode{},
-                    .display_mode = switch (try enumFromJSON(obj.get("display_style"), enum { @"fullscreen-view", @"comments-view" })) {
-                        .@"fullscreen-view" => .fullscreen,
-                        .@"comments-view" => .centered,
-                    },
-                };
+    pub fn fromJSON(jh: JsonHelper) !Page {
+        return Page{
+            .title = try jh.get("title").asString(),
+            .content = &[_]PostContext{},
+            .sidebar = try jh.get("sidebar").asArray(SidebarNode.fromJSON),
+            .display_mode = switch (try jh.get("display_style").asEnum(enum { @"fullscreen-view", @"comments-view" })) {
+                .@"fullscreen-view" => .fullscreen,
+                .@"comments-view" => .centered,
             },
-            else => return error.BadJSON,
-        }
+        };
     }
 };
 
@@ -43,17 +77,6 @@ pub const Post = struct {
     },
     actions: []const Action,
 };
-pub fn enumFromJSON(value_opt: ?std.json.Value, comptime Enum: type) !Enum {
-    const value = value_opt orelse return error.BadJSON;
-    return std.meta.stringToEnum(Enum, try stringFromJSON(value)) orelse return error.BadJSON;
-}
-pub fn stringFromJSON(value_opt: ?std.json.Value) ![]const u8 {
-    const value = value_opt orelse return error.BadJSON;
-    return switch (value) {
-        .String => |str| return str,
-        else => return error.BadJSON,
-    };
-}
 pub const Action = struct {
     text: []const u8,
 };
@@ -62,6 +85,12 @@ pub const SidebarNode = union(enum) {
         title: []const u8,
         body: []const u8,
     },
+    pub fn fromJSON(jh: JsonHelper) !SidebarNode {
+        return switch (try jh.get("kind").asEnum(enum { widget, thread })) {
+            .widget => return SidebarNode{ .sample = .{ .title = "sample", .body = "sample" } },
+            .thread => return SidebarNode{ .sample = .{ .title = "sample", .body = "sample" } },
+        };
+    }
 };
 
 const std = @import("std");
@@ -77,7 +106,7 @@ pub fn initSample(alloc: *std.mem.Allocator) Page {
         @panic("error");
     };
 
-    return Page.fromJSON(json_res.root) catch |e| {
+    return Page.fromJSON(JsonHelper{ .value = json_res.root, .alloc = alloc }) catch |e| {
         std.log.emerg("JSON error: {}", .{e});
         @panic("error");
     };
