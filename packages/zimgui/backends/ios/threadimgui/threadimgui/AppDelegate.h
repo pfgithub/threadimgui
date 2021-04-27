69780dfeb13f499fe584101cdfdbe6bedc384cef
pfg <omegariisthebest@gmail.com>
2021-04-27 13:24:27 -0700

    starting on ios (with xcode atm)

---
+++ packages/zimgui/backends/ios/threadimgui/threadimgui/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  threadimgui
+//
+//  Created by pfg on 4/27/21.
+//  Copyright © 2021 threadimgui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ packages/zimgui/backends/ios/threadimgui/threadimgui/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  threadimgui
+//
+//  Created by pfg on 4/27/21.
+//  Copyright © 2021 threadimgui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
