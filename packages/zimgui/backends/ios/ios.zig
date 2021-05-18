faa99a0c0449b4b992ac6d92bcd75eab8e91864b
pfg <omegariisthebest@gmail.com>
2021-05-18 00:10:49 -0700

    start rendering actual content on ios

--- packages/zimgui/backends/ios/ios.zig
+++ packages/zimgui/backends/ios/ios.zig
@@ -5,7 +5,14 @@ const structures = @import("../../structures.zig");
 // const CGFloat = ; // switch(os) {.watchos => double, else => float}
 const CGFloat = f64;
 
-pub const Context = struct {};
+pub const Context = struct {
+    ref: *CData,
+
+    pub fn renderRectangle(ctx: Context, color: structures.Color, rect: structures.Rect, radius: f64) void {
+        // std.log.info("rounded rect called!", .{});
+        ctx.ref.objc_draw_rect(rect.x, rect.y, rect.w, rect.h, color.r, color.g, color.b, color.a);
+    }
+};
 
 pub const RerenderRequest = struct {
     pub fn queueDraw(rr: @This()) void {
@@ -15,7 +22,10 @@ pub const RerenderRequest = struct {
 
 extern fn c_main(argc: c_int, argv: [*]const [*]const u8, data: *const backend.OpaquePtrData) c_int;
 
+var global_data_ptr: *const backend.OpaquePtrData = undefined;
+
 pub fn startBackend(data: *const backend.OpaquePtrData) error{Failure}!void {
+    global_data_ptr = data;
     _ = c_main(@intCast(c_int, std.os.argv.len), std.os.argv.ptr, data);
     unreachable; // never returns unfortunately. that means eg leak detection will never occur. TODO.
     // fake event
@@ -27,5 +37,11 @@ const CData = opaque{
 };
 
 export fn zig_render(ref: *CData) void {
-    ref.objc_draw_rect(25, 25, 100, 100, 1.0, 0.5, 0.0, 1.0);
+    const data = global_data_ptr; // todo make this an arg
+
+    // todo actual screen size
+    data.pushEvent(.{ .resize = .{ .x = 0, .y = 0, .w = 200, .h = 200 } }, .{}, data.data);
+    data.renderFrame(Context{ .ref = ref }, .{}, data.data);
+
+    // ref.objc_draw_rect(25, 25, 100, 100, 1.0, 0.5, 0.0, 1.0);
 }
